74cbea112bf018beefce5148fe2022a4a6883c1c
muNeves3 <murilogrilosn@gmail.com>
2020-08-06 21:50:15 -0300

    :package: main code

---
+++ index.js
@@ -0,0 +1,22 @@
+const webp = require("webp-converter");
+var fs = require("fs");
+var domain = require("domain").create();
+
+fs.readdir("./arquivos", function (error, files) {
+  for (let i = 0; i < files.length; i++) {
+    const result = webp.dwebp(
+      `arquivos/${files[i]}`,
+      `assets/arquivo${i}.jpg`,
+      "-o"
+    );
+    result.then((response) => {
+      console.log(response);
+    });
+  }
+
+  console.log(files);
+});
+
+domain.on("error", function (erros) {
+  console.log(erros);
+});
