83c30bd2967f93a0fbd17efd2bf27bf13b59985e
Murilo de Souza Neves <murilogrilosn@gmail.com>
2020-08-06 22:42:06 -0300

    :bug: bug: fixed now you can put any files

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const webp = require("webp-converter");
+const path = require("path");
 var fs = require("fs");
 var domain = require("domain").create();
 
-fs.readdir("./arquivos", function (error, files) {
+fs.readdir("arquivos", function (error, files) {
   for (let i = 0; i < files.length; i++) {
-    const result = webp.dwebp(
-      `arquivos/${files[i]}`,
-      `assets/arquivo${i}.jpg`,
-      "-o"
-    );
-    result.then((response) => {
-      console.log(response);
-    });
+    if (path.extname(files[i]) == ".webp") {
+      const result = webp.dwebp(
+        `arquivos/${files[i]}`,
+        `assets/arquivo${i}.jpg`,
+        "-o"
+      );
+      result.then((response) => {
+        console.log(response);
+      });
+    }
   }
 
   console.log(files);
